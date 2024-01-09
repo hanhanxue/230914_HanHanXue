1ade1da9ea6ef88a10fca87d0ee7bd127d4199c1
hanhanxue <hanhanxue.hhx@gmail.com>
2024-01-08 20:16:45 -0500

    tweaked bio

--- src/components/info/BioSection.tsx
+++ src/components/info/BioSection.tsx
@@ -9,7 +9,8 @@ const BioSection = () => {
 
         <div className={`${styles.sectionGrid}`}> 
                 <div className={`${styles.bioFrame}`}>
-                <p className={`body-2xl ${styles.bio}`}>Han Han Xue designs and develops digital products for deep tech startups. He currently works on simulation software for heavy industries.</p>
+                <p className={`body-2xl ${styles.bio}`}>Han Han Xue designs and develops digital products for deep tech startups.</p>
+                <p className={`body-2xl ${styles.bio}`}>Previously working on developer tooling for autonomous vehicles at Lyft and Woven by Toyota, he currently works on simulation software tailored for aerospace and other heavy industries.</p>
 
                 <ul className={`body-xl ${styles.sub}`}>
                     {/* <li>Born in Shenzhen, China</li> */}
